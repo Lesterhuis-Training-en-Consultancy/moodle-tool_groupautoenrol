84e40d5d27e2a80e29fd04efb9ce9eaa3af3b2da
Pascal <pascal.maury@gmail.com>
2016-04-11 18:27:06 +0200

    Fix lang AMOS and url construction
    Version 2016041101

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016031801;    // The (date) version of this module + 2 extra digital for daily versions.
+$plugin->version = 2016041101;    // The (date) version of this module + 2 extra digital for daily versions.
                                   // This version number is displayed into /admin/forms.php.
 $plugin->requires = 2013040500;   // Requires this Moodle version - at least 2.0.
 $plugin->cron = 0;
