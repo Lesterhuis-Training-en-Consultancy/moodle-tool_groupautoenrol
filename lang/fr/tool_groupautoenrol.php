bb1cc13f9b5d74a035f5bba02bd20ceebe9042c2
Pascal <pascal.maury@gmail.com>
2016-04-11 18:19:42 +0200

    Fix lang string to work with AMOS

--- lang/fr/tool_groupautoenrol.php
+++ lang/fr/tool_groupautoenrol.php
@@ -27,5 +27,4 @@ $string['auto_group_form_rand_method'] = 'Inscrire les étudiants aléatoirement
 $string['auto_group_form_usegroupslist'] = 'Limiter les inscriptions aux groupes sélectionnés';
 $string['auto_group_form_groupslist'] = 'Sélectionner les groupes :';
 $string['auto_group_form_rolelist']  = 'Choisissez qui doit être auto-inscrits :';
-$string['auto_group_enrol_form_no_group_found']  = 'Vous devez d\'abord <a href="'.$CFG->wwwroot.'/group/index.php?id={$a}">créer des groupes</a>.';
-
+$string['auto_group_enrol_form_no_group_found']  = 'Vous devez d\'abord créer des groupes !';
